89990890d5ac9b325e8c6cf8583b94183f6be31f
Ali <kledisali88@gmail.com>
2024-06-08 16:35:58 +0200

    added somethings

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -2,19 +2,146 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use App\Models\BaseModel;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Http\Resources\BaseResource;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class ResourceController extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
 
+    protected const WITH_AUTHORIZATION = false;
+    protected const MODEL_NAMESPACE = 'App\Models';
+    protected BaseModel $model;
+
+    protected string $viewFolderName;
+
+    protected string $resourceLabel;
+
+    protected string $resourceName;
+
+    protected array $validations = [];
+
     public function __construct()
     {
-        $resourceName = class_basename(static::class);
+        $this->model = $this->getModel();
 
-        // $this->authorizeResource(Employee::class);
+        $this->viewFolderName = $this->viewFolderName ?? $this->model->getPlural();
+        $this->resourceLabel = $this->resourceLabel ?? $this->model->getPlural(true);
+
+        if (static::WITH_AUTHORIZATION) {
+            $this->authorizeResource(class_basename($this->model));
+        }
     }
 
+    protected function getModel(): BaseModel 
+    {
+        $modelName = static::MODEL_NAMESPACE . '\\' . str_replace('Controller', '', class_basename(static::class));
+        return new $modelName();
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $models = $this->model
+            ->when(
+                $request->query('query'),
+                fn (Builder $query) => $query->whereAny(
+                    $this->model->getSearchFiels(), 'like', '%' . $request->query('query') . '%'
+                    )
+            )
+            ->latest()
+            ->latest('id')
+            ->paginate()
+            ->withQueryString();
+
+        return inertia("$this->viewFolderName/Index", [
+            //ex. employees
+            $this->resourceLabel => $this->model->getResourceName()::collection($models),
+            'query' => $request->query('query'),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $data = [];
+
+        foreach ($this->model->relations as $relation) {
+            /** @var BaseModel $relation */
+            $data[$relation->getPlural(true)] = fn() => $relation->getResourceName()::collection($relation->all());
+        }
+
+        return inertia("$this->viewFolderName/Create", $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate($this->validations);
+        
+        $model = $this->model->create($data);
+
+        return redirect($model->showRoute());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request, $modelId)
+    {
+        $model = $this->findResource($modelId);
+
+        if (! Str::endsWith($model->showRoute(), $request->path())) {
+            return redirect($model->showRoute($request->query()), status: 301);
+        }
+
+        if ($model->relations) {
+            $model->load(...$model->relations);
+        }
+
+        return inertia("$this->viewFolderName/Show", [
+            Str::singular($this->resourceLabel) => fn () => $this->model->getResourceName()::make($model),
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $modelId)
+    {
+        $data = $request->validate(array_intersect_key($this->validations, $request->all()));
+
+        $this->findResource($modelId)->update($data);
+
+        return redirect($this->model->showRoute(['page' => $request->query('page')]))
+            ->banner($this->model . ' updated.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Request $request, $modelId)
+    {
+        $this->findResource($modelId)->delete();
+    
+        return redirect($this->model->showRoute(['page' => $request->query('page')]))
+            ->banner(class_basename($this->model) . ' deleted.');
+    }
+
+    protected function findResource($modelId): BaseModel 
+    {
+        return $this->model->findOrFail($modelId);
+    }
 }
--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -2,11 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 
 abstract class BaseModel extends Model
 {
+    protected const RESOURCE_NAMESPACE = 'App\Http\Resources';
+
     protected $addToGet = ['relations'];
+
+    protected $searchFields = [];
+
+    public function getSearchFields(): array 
+    {
+        return $this->searchFields;
+    }
+
+    public function getPlural(bool $lowerCase = false): string 
+    {
+        $plural = Str::plural(static::class);
+
+        if ($lowerCase) {
+            return Str::lower($plural);            
+        }
+
+        return $plural;
+    }
 
     public function __get($key) 
     {
@@ -17,6 +38,21 @@ abstract class BaseModel extends Model
         if (in_array($key, $this->addToGet)) {
             return $this->$key;
         }
+    }
 
+    public function showRoute(array $parameters = [])
+    {
+        return route($this->getPlural() . '.show', [$this, $this->name ? Str::slug($this->name) : $this->id, ...$parameters]);
+    }
+
+    public function getResourceName(): string 
+    {
+        $resourceName = static::RESOURCE_NAMESPACE . '\\' . class_basename($this->model);
+
+        if (class_exists($resourceName)) {
+            return $resourceName;
+        }
+
+        return static::RESOURCE_NAMESPACE . '\\BaseResource';
     }
 }
