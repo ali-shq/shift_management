f100601824bf608c4476c6469c68def7371babea
hhysa <h.hysa02@gmail.com>
2024-07-02 20:17:58 +0200

    --add: navigation banner for guest user

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,7 +1,14 @@
 <script setup>
-import { Head, Link } from "@inertiajs/vue3";
 import ApplicationLogo from "@/Components/ApplicationLogo.vue";
-import AppLayout from "@/Layouts/AppLayout.vue";
+import { ref } from "vue";
+import { Head, Link, router, usePage } from "@inertiajs/vue3";
+import ApplicationMark from "@/Components/ApplicationMark.vue";
+import Banner from "@/Components/Banner.vue";
+import Dropdown from "@/Components/Dropdown.vue";
+import DropdownLink from "@/Components/DropdownLink.vue";
+import NavLink from "@/Components/NavLink.vue";
+import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
+import ConfirmationModalWrapper from "@/Components/ConfirmationModalWrapper.vue";
 defineProps({
     canLogin: Boolean,
     canRegister: Boolean,
@@ -11,9 +18,55 @@ defineProps({
 </script>
 
 <template>
-    <Head title="ShiftFacile"  />
-    <AppLayout>
-    <div
+    <Head title="ShiftFacile - Easy Scheduling, Happy Team" />
+    <Banner />
+    <div class="min-h-screen bg-gray-100">
+        <nav class="border-b border-gray-100 bg-white">
+            <!-- Primary Navigation Menu -->
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div class="flex h-16 justify-between">
+                    <div class="flex">
+                        <!-- Logo -->
+                        <div class="flex shrink-0 items-center">
+                            <Link>
+                                <ApplicationMark class="block h-9 w-auto" />
+                            </Link>
+                        </div>
+
+                        <!-- Navigation Links -->
+                        <div
+                            v-if="canLogin"
+                            class="z-10 p-6 text-right sm:fixed sm:right-0 sm:top-0"
+                        >
+                            <Link
+                                v-if="$page.props.auth.user"
+                                :href="route('dashboard')"
+                                class="font-semibold text-gray-600 hover:text-gray-900 focus:rounded-sm focus:outline focus:outline-2 focus:outline-green-500 dark:text-green-400 dark:hover:text-black"
+                                >Dashboard</Link
+                            >
+
+                            <template v-else>
+                                <Link
+                                    :href="route('login')"
+                                    class="font-semibold text-gray-600 hover:text-gray-900 focus:rounded-sm focus:outline focus:outline-2 focus:outline-green-500 dark:text-green-400 dark:hover:text-black"
+                                    >Log in</Link
+                                >
+
+                                <Link
+                                    v-if="canRegister"
+                                    :href="route('register')"
+                                    class="ml-4 font-semibold text-gray-600 hover:text-gray-900 focus:rounded-sm focus:outline focus:outline-2 focus:outline-red-500 dark:text-green-400 dark:hover:text-black"
+                                    >Register</Link
+                                >
+                            </template>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <!-- <div
         class="relative min-h-screen bg-center selection:bg-green-500 selection:text-white sm:flex sm:items-center sm:justify-center"
     >
         <div
@@ -46,10 +99,7 @@ defineProps({
         <div class="mx-auto max-w-7xl p-6 lg:p-8">
             <ApplicationLogo />
         </div>
-    </div>
-</AppLayout>
+    </div> -->
 </template>
 
-<style>
-
-</style>
+<style></style>
