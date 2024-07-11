3ce53276944184601b5eedcfe13c6182cea10983
hhysa <h.hysa02@gmail.com>
2024-07-11 22:39:02 +0200

    --added animations to text

--- resources/js/Pages/Guest-Pages/HeroSection.vue
+++ resources/js/Pages/Guest-Pages/HeroSection.vue
@@ -1,32 +1,51 @@
 <script setup>
-import HeroSectionIcon from '@/Components/icon-components/HeroSectionIcon.vue';
-import { Link } from '@inertiajs/vue3';
-
-
-
+import HeroSectionIcon from "@/Components/icon-components/HeroSectionIcon.vue";
+import { Link } from "@inertiajs/vue3";
 </script>
 <template>
     <!-- hero section goes here -->
     <section class="bg-grey-300 dark:bg-white-900 h-screen">
-    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 h-full">
-        <div class="mr-auto place-self-center lg:col-span-7">
-            <h1 class="max-w-2xl py-4 mb-4 text-4xl font-extrabold tracking-tight leading-none sm:text-5xl xl:text-6xl dark:text-fontColorPrimary">Employee scheduling tool for your business</h1>
-            <p class="max-w-2xl mb-6 font-light text-fontColorThird lg:mb-8 md:text-lg lg:text-xl dark:text-fontColorThird">From scheduling to complex adjustments and employee fair assign, ShiftFacile comes to help you.</p>
-            <div class="flex flex-col sm:flex-row">
-                <Link :href="route('login')" class="bg-gradient-to-r from-fontColorPrimary to-fontColorSecondary inline-flex items-center justify-center px-5 py-3 mb-3 sm:mb-0 sm:mr-3 text-base font-medium text-center text-white rounded-lg bg-fontColorPrimary hover:bg-fontColorSecondary focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                    Get started
-                    <svg class="w-5 h-5 ml-2 -mr-1 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a 1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a 1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                </Link>
-                
+        <div
+            class="mx-auto grid h-full max-w-screen-xl px-4 py-8 lg:grid-cols-12 lg:gap-8 lg:py-16 xl:gap-0"
+        >
+            <div class="mr-auto place-self-center lg:col-span-7">
+                <h1
+                    class="animate-fade-in-right mb-4 max-w-2xl py-4 text-4xl font-extrabold leading-none tracking-tight sm:text-5xl xl:text-6xl dark:text-fontColorPrimary"
+                >
+                    Employee scheduling tool for your business
+                </h1>
+                <p
+                    class="animate-fade-in-left mb-6 max-w-2xl font-light text-fontColorThird md:text-lg lg:mb-8 lg:text-xl dark:text-fontColorThird"
+                >
+                    From scheduling to complex adjustments and employee fair
+                    assign, ShiftFacile comes to help you.
+                </p>
+                <div class="flex flex-col sm:flex-row">
+                    <Link
+                        :href="route('login')"
+                        class="animate-shake-slow-repeat mb-3 inline-flex items-center justify-center rounded-lg bg-fontColorPrimary bg-gradient-to-r from-fontColorPrimary to-fontColorSecondary px-5 py-3 text-center text-base font-medium text-white hover:bg-fontColorSecondary focus:ring-4 focus:ring-primary-300 sm:mb-0 sm:mr-3 dark:focus:ring-fontColorPrimary "
+                    >
+                        Get started
+                        <svg
+                            class="-mr-1 ml-2 h-5 w-5"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M10.293 3.293a1 1 0 011.414 0l6 6a 1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a 1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                clip-rule="evenodd"
+                            ></path>
+                        </svg>
+                    </Link>
+                </div>
+            </div>
+            <div class="hidden pt-8 lg:col-span-5 lg:mt-0 lg:flex">
+                <HeroSectionIcon />
             </div>
         </div>
-        <div class="hidden lg:mt-0 lg:col-span-5 lg:flex  pt-8 ">
-            <HeroSectionIcon/>
-        </div>                
-    </div>
-</section>
-
-
+    </section>
 </template>
 
 <style scoped></style>
