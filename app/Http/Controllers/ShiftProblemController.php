f364c7d22267601a6662841a90d5974a6a128b23
Ali <kledisali88@gmail.com>
2024-07-13 21:53:37 +0200

    small addition to shift problem controller

--- app/Http/Controllers/ShiftProblemController.php
+++ app/Http/Controllers/ShiftProblemController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Employee;
+use App\Models\Place;
 use App\Models\ShiftProblem;
+use App\Models\Skill;
+use App\Models\Spot;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
@@ -19,11 +22,40 @@ class ShiftProblemController extends ResourceController
 
     public function generateNew(Request $request)
     {
-        $startDate = $request->input('start_date');
-        $endDate = $request->input('end_date');
+        $startDate = $request->input('start_date_time');
+        $endDate = $request->input('end_date_time');
+
+        $shiftProblem = new ShiftProblem(['start_date_time' => $startDate, 'end_date_time' => $endDate]);
+
+        $shiftProblem->save();
 
         $employees = Employee::getActive($startDate, $endDate);
 
+        $places = Place::with(['shifts', 'skills'])->whereIsActive(true)->get();
+
+        $spots = [];
+
+        foreach ($places as $place) {
+            foreach ($place->skills as $skill) {
+                foreach ($place->shifts as $shift) {
+                    $neededEmployees = $skill->pivot->needed_employees;
+                    while($neededEmployees-- > 0) {
+                        $spots[] = $shiftProblem->spots()->create([
+                            'shift_id' => $shift->id,
+                            'place_id' => $place->id,
+                            'skill_id' => $skill->id,
+                            'start_date_time' => $startDate,
+                            'end_date_time' => $endDate,
+                        ]);
+                    }
+                }
+            }
+        }
+
+        $shiftProblem->all_employees = $employees->pluck('id')->toJson();
+        $shiftProblem->all_places = $places->pluck('id')->toJson();
+
+        $shiftProblem->save();
         // var_dump($employees->count());
     }
 }
--- database/migrations/2024_05_06_135151_create_shift_problems_table.php
+++ database/migrations/2024_05_06_135151_create_shift_problems_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('shift_problems', function (Blueprint $table) {
             $table->id();
+            $table->text('note');
             $table->dateTime('start_date_time');
             $table->dateTime('end_date_time');
             $table->json('all_places')->default('{}');
