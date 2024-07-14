eb0a8cad7819bac11108b25e7b03ad6ebb713559
hhysa <h.hysa02@gmail.com>
2024-07-14 23:57:01 +0200

    --add icon for nav drawer

--- resources/js/Components/NavigationDrawer.vue
+++ resources/js/Components/NavigationDrawer.vue
@@ -15,14 +15,25 @@ export default {
 };
 </script>
 <template>
-    <div
+     <button @click="toggleDrawer" class="p-2" >
+      <!-- Icon for the menu (you can replace it with any icon you prefer) -->
+      <svg v-if="!isDrawerOpen" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+      </svg>
+      <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+      </svg>
+    </button>
+    <!-- <div
         :class="{ block: isOpen, hidden: !isOpen }"
         class="fixed inset-0 z-50 flex"
     >
+    
         <div
             class="fixed inset-0 bg-gray-600 opacity-75"
             @click="toggleDrawer"
         ></div>
+        
         <div class="relative w-64 bg-white">
             <div class="absolute right-0 top-0 -mr-12 pt-2">
                 <button
@@ -52,7 +63,7 @@ export default {
                 </Link>
             </nav>
         </div>
-    </div>
+    </div> -->
 </template>
 
 <style scoped></style>
