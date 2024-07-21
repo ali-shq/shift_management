6f66be06a5939cc97dac20d292c5186ef9cd5fd5
hhysa <h.hysa02@gmail.com>
2024-07-21 23:00:53 +0200

    fixed menu position

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -26,47 +26,65 @@ function toggleDrawer() {
                 <!-- Primary Navigation Menu -->
                 <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                     <div class="flex h-16 items-center justify-between">
-                        <div class="flex">
-                            <!-- Logo -->
-                            <div class="flex shrink-0 items-center">
-                                <Link>
-                                    <ApplicationLogo class="block h-9 w-auto" />
-                                </Link>
-                            </div>
-                            <div class="hidden md:flex md:items-center md:space-x-4">
-                    <a href="#" class="text-gray-700 hover:text-gray-900">Home</a>
-                    <a href="#about" class="text-gray-700 hover:text-gray-900">About</a>
-                    <a href="#services" class="text-gray-700 hover:text-gray-900">Services</a>
-                    <a href="#contact" class="text-gray-700 hover:text-gray-900">Contact</a>
-                </div>
-                            <!-- Navigation Links -->
-                            <div class="flex items-center space-x-4">
-                                <div
-                                    v-if="canLogin"
-                                    class="hidden p-6 text-right md:block"
-                                >
-                                    <Link
-                                        v-if="$page.props.auth.user"
-                                        :href="route('dashboard')"
-                                        class="font-semibold text-gray-600 hover:text-gray-900 focus:rounded-sm focus:outline focus:outline-2 focus:outline-green-500 dark:text-green-400 dark:hover:text-black"
-                                        >Dashboard</Link
-                                    >
-
-                                    <template v-else>
-                                        <Link
-                                            :href="route('login')"
-                                            class="hidden rounded p-2 font-semibold text-gray-600 outline hover:text-gray-900 focus:rounded-sm focus:outline focus:outline-2 focus:outline-fontColorPrimary md:inline-block dark:text-fontColorPrimary dark:hover:text-fontColorSecondary"
-                                            >Log in</Link
-                                        >
-                                    </template>
-                                </div>
-                            </div>
-
-                            <NavigationDrawer
-                                :isOpen="drawerOpen"
-                                @toggle-drawer="toggleDrawer"
-                            />
+                        <!-- Logo -->
+                        <div class="flex-shrink-0">
+                            <Link>
+                                <ApplicationLogo class="block h-9 w-auto" />
+                            </Link>
                         </div>
+                        <!-- Navigation Links -->
+                        <div
+                            class="ml-auto hidden md:flex md:items-center md:space-x-4"
+                        >
+                            <a
+                                href="#"
+                                class="text-gray-700 hover:text-gray-900"
+                                >Home</a
+                            >
+                            <a
+                                href="#about"
+                                class="text-gray-700 hover:text-gray-900"
+                                >About</a
+                            >
+                            <a
+                                href="#services"
+                                class="text-gray-700 hover:text-gray-900"
+                                >Services</a
+                            >
+                            <a
+                                href="#contact"
+                                class="text-gray-700 hover:text-gray-900"
+                                >Contact</a
+                            >
+                        </div>
+
+                        <!-- Navigation Links -->
+                        <div class="flex items-center space-x-4">
+                            <div
+                                v-if="canLogin"
+                                class="hidden p-6 text-right md:block"
+                            >
+                                <Link
+                                    v-if="$page.props.auth.user"
+                                    :href="route('dashboard')"
+                                    class="font-semibold text-gray-600 hover:text-gray-900 focus:rounded-sm focus:outline focus:outline-2 focus:outline-green-500 dark:text-green-400 dark:hover:text-black"
+                                    >Dashboard</Link
+                                >
+
+                                <template v-else>
+                                    <Link
+                                        :href="route('login')"
+                                        class="hidden rounded p-2 font-semibold text-gray-600 outline hover:text-gray-900 focus:rounded-sm focus:outline focus:outline-2 focus:outline-fontColorPrimary md:inline-block dark:text-fontColorPrimary dark:hover:text-fontColorSecondary"
+                                        >Log in</Link
+                                    >
+                                </template>
+                            </div>
+                        </div>
+
+                        <NavigationDrawer
+                            :isOpen="drawerOpen"
+                            @toggle-drawer="toggleDrawer"
+                        />
                     </div>
                 </div>
             </nav>
